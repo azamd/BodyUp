c07adfb3b553c7ee6c29a2e2903c5e2a62cc9aeb
mednsi <blacksn0wlabs1337@gmail.com>
2021-04-28 01:06:40 +0100

    integration
    rana modules

--- BODYUPWEB/src/Controller/admin/GymController.php
+++ BODYUPWEB/src/Controller/admin/GymController.php
@@ -38,15 +38,68 @@ class GymController extends AbstractController
         }
 
 
-
-
-
-
-
-
-
-
-
         return $this->render('admin/gym/index.html.twig',['gyms' => $gyms,'form' => $form->createView()]);
     }
+
+    /**
+     * @Route("/gym/modify={id}", name="gym_modify")
+     */
+    public function show(Request $request,$id)
+    {
+        $Gyms = $this->getDoctrine()->getRepository(Gym::class)->findAll();
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $Gym = $entityManager->getRepository(Gym::class)->find($id);
+        $form = $this->createForm(GymType::class,$Gym);
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted()&& $form->isValid())
+        {
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $data = $form->getData();
+
+            /** @var Symfony\Component\HttpFoundation\File\UploadedFile $file */
+            $img = $form->get('image')->getData();
+            //var_dump($data);die();
+            $fileName = md5(uniqid()).'.'.$img->guessExtension();
+            $img->move($this->getParameter('image_directory'), $fileName);
+            $data->setImage($fileName);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('gym');
+
+        }
+
+        return $this->render("admin/gym/index.html.twig", [
+            "form" => $form->createView(),'gym' => $Gym,'gyms' => $Gyms
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/gym/delete/{id}",name="delete_gym")
+
+     */
+    public function delete(Request $request, $id)
+    {
+        $gym = $this->getDoctrine()->getRepository(Gym::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($gym);
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->send();
+
+        return $this->redirectToRoute('gym');
+    }
+
+
+
 }
