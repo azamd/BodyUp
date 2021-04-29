e7e73aaffc7cc6510040a836a72c106dec10b767
mednsi <blacksn0wlabs1337@gmail.com>
2021-04-29 01:10:02 +0100

    integration bundles

--- BODYUPWEB/public/back/js/custom_js/user_payment.js
+++
@@ -1,8 +0,0 @@
-"use-strict"
-$(document).ready(function(){
-new Card({
-    form: document.querySelector('form'),
-    container: '.card-wrapper'
-});
-$(".nameuser").on("keyup", function(){var user_name=$(".nameuser").val();$(".jp-card-name").text(user_name);});
-});
--- BODYUPWEB/public/front/js/menu.js
+++
@@ -1,9 +0,0 @@
-$("a.open_close").on("click", function() {
-    $(".main-menu").toggleClass("show"), $(".layer").toggleClass("layer-is-visible")
-}), $("a.show-submenu").on("click", function() {
-    $(this).next().toggleClass("show_normal")
-}), $("a.show-submenu-mega").on("click", function() {
-    $(this).next().toggleClass("show_mega")
-}), $(window).width() <= 480 && $("a.open_close").on("click", function() {
-    $(".cmn-toggle-switch").removeClass("active")
-});
